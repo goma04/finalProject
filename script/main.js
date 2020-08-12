ebaed6886b32d6a98010ba2302b4c2fd4917ec23
goma04 <47868060+goma04@users.noreply.github.com>
2020-08-13 00:21:50 +0200

    Gépről feltöltve, 2. verzió

--- script/main.js
+++ script/main.js
@@ -1,5 +1,44 @@
-let lastElement = document.querySelector(".main").lastElementChild;
-let secondLast = lastElement.before(lastElement);
 
 
-console.log(secondLast);
+
+let oldY = 0;
+function navbarSettings(){
+    let down = false;
+    let currentY = window.scrollY;    
+    if(oldY > currentY){
+        down = true;
+    }
+    oldY = currentY;
+    let table = document.querySelector(".navbar");
+    
+    if(down==false){
+        table.style.opacity = "40%";
+    } 
+    else{
+        table.style.opacity = "90%";
+    }  
+
+    //megnézi hogy mekkora az oldal magassága, aszerint változtatja a navbar színét
+    let pageHeight = document.querySelector('body').lastElementChild.getBoundingClientRect().bottom;
+    let whereIsUser = currentY / pageHeight;
+
+    if(whereIsUser<0.2){
+        table.style.backgroundColor ="#f87373";
+    }
+    else if(whereIsUser<0.4){
+        table.style.backgroundColor ="#F67D7D";
+    }
+    else if(whereIsUser<0.6){
+        table.style.backgroundColor ="#F6A4A4";
+    }
+    else if(whereIsUser<0.8){
+        table.style.backgroundColor ="#F8D4D4";
+    }
+    else{
+        table.style.backgroundColor ="#85f7f7";
+    }
+
+}
+
+window.onscroll = navbarSettings;
+
